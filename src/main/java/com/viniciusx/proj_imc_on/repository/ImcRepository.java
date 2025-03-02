45cbf10df18d0dd6c1c0295b9531eb50ca78beac
Vinicius Ribeiro <vini.silvaa2002@gmail.com>
2025-03-02 15:48:48 -0300

    Modificado metodo reset id do banco

--- src/main/java/com/viniciusx/proj_imc_on/repository/ImcRepository.java
+++ src/main/java/com/viniciusx/proj_imc_on/repository/ImcRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ImcRepository extends JpaRepository<Imc, Integer> {
 
+    @Transactional
     @Modifying
     @Query(value = "ALTER SEQUENCE imc_id_seq RESTART WITH 1", nativeQuery = true)
     void resetarSequencia();
